9c87a390f0e8355814d3b1aa55f8524964b2be5f
Niel Conradie <nielconradie93@gmail.com>
2022-06-15 11:48:23 +0200

    Update run.py
    Added restart method to run.py file.

--- turtle-racer/run.py
+++ turtle-racer/run.py
@@ -4,39 +4,45 @@ from turtle_racer import TurtleRacer
 
 
 def run():
-    """ Turtle Racer. """
+    """ Turtle Racer """
     run = TurtleRacer()
 
     # Display screen.
     run.create_screen()
     # Display heading.
     run.draw_heading()
-    # Display track.
-    run.draw_track()
-    # Display finish line.
-    run.draw_finish_line()
 
-    # Create turtle racers.
-    red = run.turtle_red()
-    orange = run.turtle_orange()
-    yellow = run.turtle_yellow()
-    lime = run.turtle_lime()
-    green = run.turtle_green()
-    cyan = run.turtle_cyan()
-    blue = run.turtle_blue()
-    purple = run.turtle_purple()
-    magenta = run.turtle_magenta()
-    pink = run.turtle_pink()
+    while True:
+        # Display track.
+        run.draw_track()
+        # Display finish line.
+        run.draw_finish_line()
 
-    # Pause for a second.
-    time.sleep(1)
+        # Create turtle racers.
+        red = run.turtle_red()
+        orange = run.turtle_orange()
+        yellow = run.turtle_yellow()
+        lime = run.turtle_lime()
+        green = run.turtle_green()
+        cyan = run.turtle_cyan()
+        blue = run.turtle_blue()
+        purple = run.turtle_purple()
+        magenta = run.turtle_magenta()
+        pink = run.turtle_pink()
 
-    # Starting the race.
-    run.race(red, orange, yellow, lime, green,
-             cyan, blue, purple, magenta, pink)
-    # Win conditions.
-    run.win_condition(red, orange, yellow, lime, green,
-                      cyan, blue, purple, magenta, pink)
+        # Pause for a second.
+        time.sleep(1)
+
+        # Starting the race.
+        run.race(red, orange, yellow, lime, green,
+                 cyan, blue, purple, magenta, pink)
+        # Win conditions.
+        run.win_condition(red, orange, yellow, lime, green,
+                          cyan, blue, purple, magenta, pink)
+        # Requesting user input.
+        run.restart()
+
+        continue
 
 
 if __name__ == '__main__':
