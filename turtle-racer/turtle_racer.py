8410c837c890cc48e6a17844745a898a9a51c11c
Niel Conradie <nielconradie93@gmail.com>
2022-06-28 11:52:39 +0200

    Update turtle_racer.py
    Fixed small issues.

--- turtle-racer/turtle_racer.py
+++ turtle-racer/turtle_racer.py
@@ -104,8 +104,7 @@ class TurtleRacer:
             magenta.forward(random.randint(5, 15))
             pink.forward(random.randint(5, 15))
 
-    @staticmethod
-    def win_condition(run, red, orange, yellow, lime, green, cyan, blue, purple, magenta, pink):
+    def win_condition(self, red, orange, yellow, lime, green, cyan, blue, purple, magenta, pink):
         """ Win conditional statements. """
         # Red turtle wins.
         if red.xcor() > orange.xcor() and red.xcor() > yellow.xcor() and \
@@ -115,7 +114,7 @@ class TurtleRacer:
                 red.xcor() > pink.xcor():
 
             print("\nRed Turtle Wins!")
-            run.celebrate(red)
+            self.celebrate(red)
 
         # Orange turtle wins.
         if orange.xcor() > yellow.xcor() and orange.xcor() > lime.xcor() and \
@@ -125,7 +124,7 @@ class TurtleRacer:
                 orange.xcor() > red.xcor():
 
             print("\nOrange Turtle Wins!")
-            run.celebrate(orange)
+            self.celebrate(orange)
 
         # Yellow turtle wins.
         if yellow.xcor() > lime.xcor() and yellow.xcor() > green.xcor() and \
@@ -135,7 +134,7 @@ class TurtleRacer:
                 yellow.xcor() > orange.xcor():
 
             print("\nYellow Turtle Wins!")
-            run.celebrate(yellow)
+            self.celebrate(yellow)
 
         # Lime turtle wins.
         if lime.xcor() > green.xcor() and lime.xcor() > cyan.xcor() and \
@@ -145,7 +144,7 @@ class TurtleRacer:
                 lime.xcor() > yellow.xcor():
 
             print("\nLime Turtle Wins!")
-            run.celebrate(lime)
+            self.celebrate(lime)
 
         # Green turtle wins.
         if green.xcor() > cyan.xcor() and green.xcor() > blue.xcor() and \
@@ -155,7 +154,7 @@ class TurtleRacer:
                 green.xcor() > lime.xcor():
 
             print("\nGreen Turtle Wins!")
-            run.celebrate(green)
+            self.celebrate(green)
 
         # Cyan turtle wins.
         if cyan.xcor() > blue.xcor() and cyan.xcor() > purple.xcor() and \
@@ -165,7 +164,7 @@ class TurtleRacer:
                 cyan.xcor() > green.xcor():
 
             print("\nCyan Turtle Wins!")
-            run.celebrate(cyan)
+            self.celebrate(cyan)
 
         # Blue turtle wins.
         if blue.xcor() > purple.xcor() and blue.xcor() > magenta.xcor() and \
@@ -175,7 +174,7 @@ class TurtleRacer:
                 blue.xcor() > cyan.xcor():
 
             print("\nBlue Turtle Wins!")
-            run.celebrate(blue)
+            self.celebrate(blue)
 
         # Purple turtle wins.
         if purple.xcor() > magenta.xcor() and purple.xcor() > pink.xcor() and \
@@ -185,7 +184,7 @@ class TurtleRacer:
                 purple.xcor() > blue.xcor():
 
             print("\nPurple Turtle Wins!")
-            run.celebrate(purple)
+            self.celebrate(purple)
 
         # Magenta turtle wins.
         if magenta.xcor() > pink.xcor() and magenta.xcor() > red.xcor() and \
@@ -195,7 +194,7 @@ class TurtleRacer:
                 magenta.xcor() > purple.xcor():
 
             print("\nMagenta Turtle Wins!")
-            run.celebrate(magenta)
+            self.celebrate(magenta)
 
         # Pink turtle wins.
         if pink.xcor() > red.xcor() and pink.xcor() > orange.xcor() and \
@@ -205,7 +204,7 @@ class TurtleRacer:
                 pink.xcor() > magenta.xcor():
 
             print("\nPink Turtle Wins!")
-            run.celebrate(pink)
+            self.celebrate(pink)
 
     @staticmethod
     def celebrate(turtle):
