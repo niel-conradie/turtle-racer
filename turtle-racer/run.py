6dc86c4bf4a31f293cea709d3331fb09571f6d3d
Niel Conradie <nielconradie93@gmail.com>
2022-06-30 13:46:13 +0200

    Update run.py
    Black code formatted file and minor cleaning.

--- turtle-racer/run.py
+++ turtle-racer/run.py
@@ -3,7 +3,7 @@ from turtle_racer import TurtleRacer
 
 
 def run():
-    """ Turtle Racer. """
+    """Turtle Racer."""
     run = TurtleRacer()
 
     # Display screen.
@@ -20,16 +20,16 @@ def run():
             run.draw_finish_line()
 
             # Create turtle racers.
-            red = run.create_turtle('red', -510, 215)
-            orange = run.create_turtle('orange', -510, 165)
-            yellow = run.create_turtle('yellow', -510, 115)
-            lime = run.create_turtle('lime', -510, 65)
-            green = run.create_turtle('green', -510, 15)
-            cyan = run.create_turtle('cyan', -510, -35)
-            blue = run.create_turtle('blue', -510, -85)
-            purple = run.create_turtle('purple', -510, -135)
-            magenta = run.create_turtle('magenta', -510, -185)
-            pink = run.create_turtle('pink', -510, -235)
+            red = run.create_turtle("red", -510, 215)
+            orange = run.create_turtle("orange", -510, 165)
+            yellow = run.create_turtle("yellow", -510, 115)
+            lime = run.create_turtle("lime", -510, 65)
+            green = run.create_turtle("green", -510, 15)
+            cyan = run.create_turtle("cyan", -510, -35)
+            blue = run.create_turtle("blue", -510, -85)
+            purple = run.create_turtle("purple", -510, -135)
+            magenta = run.create_turtle("magenta", -510, -185)
+            pink = run.create_turtle("pink", -510, -235)
 
             # Display available color options.
             run.display_available_options()
@@ -42,11 +42,13 @@ def run():
             sleep(1)
 
             # Starting the race.
-            run.start_race(red, orange, yellow, lime, green,
-                           cyan, blue, purple, magenta, pink)
+            run.start_race(
+                red, orange, yellow, lime, green, cyan, blue, purple, magenta, pink
+            )
             # Winning turtle conditions.
             winner = run.win_condition(
-                red, orange, yellow, lime, green, cyan, blue, purple, magenta, pink)
+                red, orange, yellow, lime, green, cyan, blue, purple, magenta, pink
+            )
             # Winning turtle bet conditions.
             run.bet_condition(winner, racer, bet)
             # Display available credits.
@@ -63,5 +65,5 @@ def run():
         continue
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
